688bbcc7a708cffbf838b88241c5fb655b5e7058
Bret Cameron <bretcameron@gmail.com>
2019-02-12 23:52:53 +0000

    Update printer images and styling

--- src/components/Printer.js
+++ src/components/Printer.js
@@ -6,10 +6,17 @@ export default class Printer extends Component {
         return (
             <Fragment>
                 <div technology={this.props.technology} className="printer">
-                    <h3>{this.props.printer}</h3>
-                    <h4>{this.props.technology}</h4>
-                    <p>{this.props.description}</p>
-                    <img className="printer-img" src={this.props.image} alt={this.props.printer} />
+                    <div className="printer-text">
+                        <div className="printer-headline">
+                            <h3>{this.props.printer}</h3>
+                            <h4>{this.props.technology}</h4>
+                        </div>
+                        <hr className="printer-hr" />
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className="printer-image">
+                        <img src={this.props.image} alt={this.props.printer} />
+                    </div>
                 </div>
             </Fragment>
         );
