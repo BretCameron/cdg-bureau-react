194489a50831b539d83eee1a49e0c38c7b3db29d
Bret Cameron <bretcameron@gmail.com>
2019-02-13 15:35:03 +0000

    Re-style header and navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,6 +7,7 @@ import FAQ from './pages/FAQ';
 import ExampleParts from './pages/ExampleParts';
 import ContactUs from './pages/ContactUs';
 // import Hero from './Hero';
+// import Logo from './Replik8.svg';
 
 export default class Navigation extends Component {
     render() {
@@ -17,8 +18,8 @@ export default class Navigation extends Component {
                         <ul id="nav-list">
                             <div className="container">
                                 <li><Link to="/"><span role="img" aria-label="emoji">🏠</span> Get a Quote</Link></li>
-                                <li><Link to="/technologies"><span role="img" aria-label="emoji">🤖</span> Our Technologies</Link></li>
-                                <li><Link to="/materials"><span role="img" aria-label="emoji">🏺</span> Our Materials</Link></li>
+                                <li><Link to="/technologies"><span role="img" aria-label="emoji">🤖</span>Technologies</Link></li>
+                                <li><Link to="/materials"><span role="img" aria-label="emoji">🏺</span>Materials</Link></li>
                                 <li><Link to="/faq"><span role="img" aria-label="emoji">❓</span> FAQ</Link></li>
                                 <li><Link to="/example-parts"><span role="img" aria-label="emoji">🔩</span> Example Parts</Link></li>
                                 <li><Link to="/contact-us"><span role="img" aria-label="emoji">📝</span> Contact Us</Link></li>
